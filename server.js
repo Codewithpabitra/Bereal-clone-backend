cddf19ea72c2c3a84185b4875169fc95b6342da2
Codewithpabitra <pabitra05.maity@gmail.com>
2026-04-29 10:33:46 +0530

    fix : add cors issue in the frontend url in socket connection

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const server = http.createServer(app);
 // ✅ Attach Socket.io to the server
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      process.env.FRONTEND_URL || "*",
+    ],
     methods: ["GET", "POST"],
   },
 });
