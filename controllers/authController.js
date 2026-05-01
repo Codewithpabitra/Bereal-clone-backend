8e7f434c3d20f396f901513fcb7fbcde6494068d
Codewithpabitra <pabitra05.maity@gmail.com>
2026-05-01 12:52:04 +0530

    feat : add chnage password and delete account controlller s and routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,13 @@
 const User = require("../models/User");
 const Analytics = require("../models/Analytics");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+const Notification = require("../models/Notification");
+const SavedPost = require("../models/SavedPost");
+const Reaction = require("../models/Reaction");
+const Report = require("../models/Report");
+
+
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -54,5 +62,104 @@ exports.getMe = async (req, res) => {
     res.json(user);
   } catch (err) {
     res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+// PUT /api/auth/change-password
+exports.changePassword = async (req, res) => {
+  const { currentPassword, newPassword } = req.body;
+  try {
+    if (!currentPassword || !newPassword) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (newPassword.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.findById(req.user._id);
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Current password is incorrect" });
+    }
+
+    // Check new password is different
+    const isSame = await bcrypt.compare(newPassword, user.password);
+    if (isSame) {
+      return res.status(400).json({ message: "New password must be different" });
+    }
+
+    const hashed = await bcrypt.hash(newPassword, 10);
+    await User.findByIdAndUpdate(req.user._id, { password: hashed });
+
+    res.json({ message: "Password changed successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
+
+// DELETE /api/auth/delete-account
+exports.deleteAccount = async (req, res) => {
+  const { password } = req.body;
+  try {
+    if (!password) {
+      return res.status(400).json({ message: "Password is required to delete account" });
+    }
+
+    const user = await User.findById(req.user._id);
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+
+    const userId = req.user._id;
+
+    // Delete all user data in parallel
+    await Promise.all([
+      // Delete all posts
+      Post.deleteMany({ user: userId }),
+
+      // Delete all comments
+      Comment.deleteMany({ user: userId }),
+
+      // Delete all notifications (sent and received)
+      Notification.deleteMany({
+        $or: [{ recipient: userId }, { sender: userId }],
+      }),
+
+      // Delete all saved posts
+      SavedPost.deleteMany({ user: userId }),
+
+      // Delete analytics
+      Analytics.deleteMany({ user: userId }),
+
+      // Delete all reactions
+      Reaction.deleteMany({ user: userId }),
+
+      // Delete all reports
+      Report.deleteMany({
+        $or: [{ reporter: userId }, { reportedUser: userId }],
+      }),
+
+      // Remove from other users followers/following/blocked
+      User.updateMany(
+        {},
+        {
+          $pull: {
+            followers: userId,
+            following: userId,
+            blockedUsers: userId,
+            likedPosts: { $in: await Post.find({ user: userId }).distinct("_id") },
+          },
+        }
+      ),
+    ]);
+
+    // Finally delete the user
+    await User.findByIdAndDelete(userId);
+
+    res.json({ message: "Account deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
-const { register, login, getMe } = require("../controllers/authController");
+const {
+  register,
+  login,
+  getMe,
+  changePassword,
+  deleteAccount,
+} = require("../controllers/authController");
 const protect = require("../middleware/authMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.put("/change-password", protect, changePassword);
+router.delete("/delete-account", protect, deleteAccount);
+
 module.exports = router;
